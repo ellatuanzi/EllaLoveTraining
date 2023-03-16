b99b2ef2b71ed3fa7c79ea90e0cf6a67ab9db4b6
ellatuanzi <128073763+ellatuanzi@users.noreply.github.com>
2023-03-16 09:39:12 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,24 @@
+const loginCombos = {
+    Ella: "Ella",
+    dog: "dogpass",
+    cat: "catpass",
+    clown: "clownpass",
+};
+
+function validate(event) {
+    console.log("submit", event);
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const userExists = loginCombos.hasOwnProperty(username);
+    const rightPass = loginCombos[username] === password;
+
+    if (userExists && rightPass) {
+        alert("Login Successful");
+        window.location.href = "index.html";
+    } else {
+        alert(
+        "Sorry, your username or password was incorrect. Please double-check your login information and try again."
+        );
+    }
+}
